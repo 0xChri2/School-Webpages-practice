fc516e01051c18168cf60b90c26ccaad348aecc7
ImChri2 <chris@chris-industries.de>
2022-02-01 10:47:54 +0100

    Added 8.6 with csv input

--- Aufgabe 8.5 + 8.6/index.php
+++ Aufgabe 8.5 + 8.6/index.php
@@ -15,45 +15,60 @@
                   <option value="Nicht Deutsch">Nicht Deutsch</option> 
               </select><br />
               <input type="submit" name="submit" value="Submit!" />
+              <button><a href="list.php">Liste</a></button>
           </form>  
           <?php 
           if((isset($_POST['submit']))==true){
          // if(isset($_POST['birth']) && preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $_POST['birth']) && !empty(trim($_POST['brith'])))
           //{
-            $error = true;
+            $bp = false;
+            $bk = false;
             $message = array();
-            $birth = $_POST['birth'];
-            $birth = strtotime(date("Y-m-d", strtotime($birth)));
-            $time = strtotime(date("Y-m-d" ,time()));
 
+            $vname = $_POST['vname'];
+            $nname = $_POST['nname'];
             $nationality = $_POST['nationality'];
-
+            $birth = date("Y", strtotime($_POST['birth']));
+            $time = date("Y" , time());
+            
             $age = $time - $birth;
+            
             if($age >= 40 && $nationality == "Deutsch")
             {
                $message[] = "Sie können Bundespräsident werden.";
-               $error = false;
+               $bp = true;
             }
             if($age >= 18 && $nationality == "Deutsch")
             {
                 $message[] = "Sie können Bundeskanzler werden.";
-                $error = false;
+                $bk = true;
             }
-            
+            if($nationality != "Deutsch")
+            {
+                $message[] = "Ihre Staatsangehörigkeit muss Deutsch sein um Bundespräsident oder Bundeskanzler zu werden.";
+            }
+            if($age < 18)
+            {
+                $message[] = "Sie sind zu jung um Bundeskanzler oder Bundespräsident zu werden.";
+            }
 
 
             foreach($message as $messages)
             {   
-                echo "";
-                if($error == false)
-                {
-                    echo "<center><h1>".$messages."</h1></center>";
-                }
-                if($error == true)
-                {
                     echo"<center><h1>".$messages."</center> </h1>";
-                }
             }
+
+            if($bp == true)
+            {
+                $bpopen = fopen("bp.csv", "a+");
+                fputcsv($bpopen, array($vname, $nname, $age,";"));
+            }
+            if($bk == true)
+            {
+                $bkopen = fopen("bk.csv", "a+");
+                fputcsv($bkopen, array($vname, $nname, $age,";"));
+            }
+
           }
          
           
