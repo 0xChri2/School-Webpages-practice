44215c4125330f1934b85e6586f8f9bdc0b035c1
ImChri2 <chris@chris-industries.de>
2022-04-06 10:40:35 +0200

    finish Lottery added chose 6 numbers and see if you win

--- Lottozahlen/index.php
+++ Lottozahlen/index.php
@@ -1,5 +1,6 @@
 <?php
 require'phpqrcode/qrlib.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,27 +8,35 @@ require'phpqrcode/qrlib.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Lottozahlen</title>
 </head>
 <body>
+    <div class="start">
     <center>
     <h1>Lottzahlen</h1>
     <br />
     <form method="POST">
-        Alter<input type="number" name="Alter" placeholder="20" /><br /><br />
-        Zahl 1 <input type="number" name="Zahl1" placeholder="1" /><br /><br />
-        Zahl 2 <input type="number" name="Zahl2" placeholder="2" /><br /><br />
-        Zahl 3 <input type="number" name="Zahl3" placeholder="3" /><br /><br />
-        Zahl 4 <input type="number" name="Zahl3" placeholder="4" /><br /><br />
-        Zahl 5 <input type="number" name="Zahl3" placeholder="5" /><br /><br />
-        Zahl 6 <input type="number" name="Zahl3" placeholder="6" /><br /><br />
-        <input type="submit" value="Tippen!" name="submit" />
+        Alter <input type="number" name="Alter" placeholder="20" required /><br /><br />
+        Zahl 1 <input type="number" name="Zahl1" placeholder="12" min="1" max="49" required /><br /><br />
+        Zahl 2 <input type="number" name="Zahl2" placeholder="32" min="1" max="49" required /><br /><br />
+        Zahl 3 <input type="number" name="Zahl3" placeholder="49" min="1" max="49" required /><br /><br />
+        Zahl 4 <input type="number" name="Zahl4" placeholder="12" min="1" max="49" required /><br /><br />
+        Zahl 5 <input type="number" name="Zahl5" placeholder="5" min="1" max="49" required /><br /><br />
+        Zahl 6 <input type="number" name="Zahl6" placeholder="2" min="1" max="49" required /><br /><br />
+        <input type="submit" value="Tippen abgeben" name="submit" />
 
     </form>
     <?php
         
         if(isset($_POST['submit'])){
             $Alter = $_POST['Alter'];
+            $tipp[0] = $_POST['Zahl1'];
+            $tipp[1] = $_POST['Zahl2'];
+            $tipp[2] = $_POST['Zahl3'];
+            $tipp[3] = $_POST['Zahl4'];
+            $tipp[4] = $_POST['Zahl5'];
+            $tipp[5] = $_POST['Zahl6'];
             $Zahlen = array();
             $Zahlen[0] = rand(1,49);
             $Zahlen[1] = rand(1,49);
@@ -43,17 +52,36 @@ require'phpqrcode/qrlib.php';
                 $errormsg[] = "Du bist zu jung um Lottozahlen zu tippen!";
                 $error = true;
             }
+            if($Alter >= 120){
+                
+                $errormsg[] = "Du bist zu alt um Lottozahlen zu tippen!";
+                $error = true;
+            }
             if($error == false)
             {   
-                echo"<h2>Ihre Lottozahlen sind:</h2>";
+                $gesamtlotto = $Zahlen[0] ." ". $Zahlen[1] ." ". $Zahlen[2] ." ". $Zahlen[3] ." ". $Zahlen[4] ." ". $Zahlen[5];
+                $gesamttipp = $tipp[0] ." ". $tipp[1] ." ". $tipp[2] ." ". $tipp[3] ." ". $tipp[4] ." ". $tipp[5];
+                $qrcode = "Lotto:".$gesamtlotto. "Dein Tipp:".$gesamttipp;
+                if($gesamtlotto == $gesamttipp){
+                    echo "</h2>Glückwunsch du hast gewonnen!</h2>";
+                }
+                else{
+                    echo "<h2>Du hast leider verloren!</h2>";
+                }
+
+                echo"<h4>Die Lottozahlen sind:</h4>";
                 foreach($Zahlen as $Zahl){
                     echo $Zahl . " ";
                 }
-                $gesamtlotto = $Zahlen[0] + $Zahlen[1] + $Zahlen[2] + $Zahlen[3] + $Zahlen[4] + $Zahlen[5];
-                
-                QRcode::png($gesamtlotto,'QRCODE/Zahlen.png');
-                echo"<br /><img src='QRCODE/Zahlen.png' />";
-    
+               
+                QRcode::png($qrcode ,'QRCODE/Zahlen.png');
+                echo"</div><br /><div class='qrcode_img'><img src='QRCODE/Zahlen.png' /></div>";
+                echo "<br /><br /><div class='ending'>";
+                echo"<h4>Ihre Lottozahlen sind:</h4>";
+
+                foreach($tipp as $Zahl){
+                    echo $Zahl . " ";
+                }
             }
             else if($error == true)
             {
@@ -65,6 +93,7 @@ require'phpqrcode/qrlib.php';
         }
     
     ?>
+    </div>
     </center>
 </body>
 </html>
